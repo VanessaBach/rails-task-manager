c44915e26fe726728527731535963196be95379a
vanessabach <vanessabach.r@gmail.com>
2020-10-10 16:22:38 -0300

    acho que foi

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  
+  before_action :set_task, only: [:title, :details, :completed]
   def index
     @tasks = Task.all
   end
@@ -7,10 +7,42 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+  params.require(:task).permit(:title, :details, :completed)
+  end
 end
-
-
-# As a user, I can view the details of a task
-# As a user, I can add a new task
-# As a user, I can edit a task (mark as completed / update title & details)
-# As a user, I can remove a task
